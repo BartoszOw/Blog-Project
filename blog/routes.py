391bb3967fd17bfbbcf8226834214f1b55fb31d0
BartoszOw <owbartosz.pl@gmail.com>
2024-04-21 20:07:18 +0200

    Posts view on site

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,13 @@
 from flask import render_template
 from blog import app
+from blog.models import Entry
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc()).limit(4).all()
+    return render_template('homepage.html', all_posts=all_posts)
+
+@app.route('/post_all')
+def post_all():
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    return render_template('homepage.html', all_posts=all_posts)
