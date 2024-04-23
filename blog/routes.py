dc840a3b6ea34b36e3fed54952cc9f97fd066e1b
BartoszOw <owbartosz.pl@gmail.com>
2024-04-23 19:29:46 +0200

    Login/Logout, Deleting and Modifying

--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,17 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
 import datetime
+import functools
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args,**kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
 
 @app.route('/')
 def index():
@@ -15,10 +24,12 @@ def post_all():
     return render_template('homepage.html', all_posts=all_posts)
 
 @app.route('/new_post', methods=['GET','POST'])
+@login_required
 def create_entry():
     return manage_entry(entry_id=None)
 
 @app.route('/edit_post/<int:entry_id>', methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id):
     return manage_entry(entry_id)
 
@@ -42,3 +53,52 @@ def manage_entry(entry_id):
         errors = form.errors
     
     return render_template('entry_form.html', form=form, errors=errors)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == "POST":
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True
+            flash("You are now logged in.", category="success")
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template('login_form.html', form=form, errors=errors)
+
+
+@app.route('/logout/', methods=["GET","POST"])
+def logout():
+    if request.method == "POST":
+        session.clear()
+        flash('You are now logged out', category='danger')
+    return redirect(url_for('index'))   
+
+@app.route('/unpublished_list', methods=['GET'])
+@login_required
+def unpublished_list():
+    un_list = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+    return render_template('unpublished_list.html', un_list=un_list)
+
+
+@app.route('/delete_entry', methods=['POST'])
+
+def delete_entry():
+    entry_id = request.form.get('entry_id') 
+    if entry_id:
+        entry = Entry.query.get_or_404(entry_id)
+        db.session.delete(entry)
+        db.session.commit()
+        flash("Entry has been deleted", category="success")
+    else:
+        flash("Entry ID is missing", category="danger")
+
+    if request.referrer:
+        return redirect(request.referrer)
+    else:
+        return redirect(url_for('index'))
+    
--- config.py
+++ config.py
@@ -12,3 +12,6 @@ class Config:
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME', 'admin')
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'admin')
